a775afa7b6f3913432059dc6c5d8a9ea8dfbe84d
jpatemb <jpat.embrey@gmail.com>
2021-05-29 23:57:13 -0400

    Changed some file information

---
+++ PPWebUI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PPBL;
+using PPModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PPWebUI.Models;
+
+namespace PPWebUI.Controllers
+{
+    public class ProductController
+    {
+
+    }
+}
